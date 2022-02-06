cc309e65cdf7fcbd253567a04b6182374307fe54
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-02-06 19:03:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from pytgcalls import idle
+
+from config import SUPPORT, bot
+from config import call_py
+from Userbot.quote import arq
+
+
+async def main():
+    await call_py.start()
+    await bot.join_chat("Rockerz_Support")            
+    await bot.send_message(
+            SUPPORT,
+            "<b>Congrats!! Music Bot has started successfully!</b>",
+        )
+    print(
+        """
+    ------------------
+   | Userbot Started! POWERED BY @Rockerz_Updates |
+    ------------------
+"""
+    )
+    await idle()
+    await arq.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
